c385d10d10da9d5847fb3afd665b655ea7d275e2
Mikkel Barfod <mib010@edu.zealand.dk>
2025-08-27 11:22:54 +0200

    Add Demo class

---
+++ MyFirstGitHubClassRoomConsoleApp60/Demo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstGitHubClassRoomConsoleApp60
+{
+    public class Demo
+    {
+        private double number;
+        private int factor;
+        private bool checker;
+        private string name;
+
+        public Demo(double num, int fac, bool chec, string nam)
+        {
+            number = num;
+            factor = fac;
+            checker = chec;
+            name = nam;
+        }
+
+        public void printOutForm()
+        {
+            string message = "";
+            if (checker)
+            {
+                bool tempBool = (number * factor) % 2 == 0;
+                message = $"Number is whole: {tempBool}\n";
+            }
+            Console.WriteLine($"{name}\n{number} times {factor} is equal to: {number * factor} \n{message}");
+        }
+    }
+}
--- MyFirstGitHubClassRoomConsoleApp60/Program.cs
+++ MyFirstGitHubClassRoomConsoleApp60/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using MyFirstGitHubClassRoomConsoleApp60;
+
+Console.WriteLine("Hello, World!\n");
+
+Demo firstDemo = new Demo(2, 8, true, "First test");
+Demo secondDemo = new Demo(3.12, 16, false, "Second test");
+Demo thirdDemo = new Demo(8.1234, 22, true, "Third test");
+
+firstDemo.printOutForm();
+secondDemo.printOutForm();
+thirdDemo.printOutForm();
